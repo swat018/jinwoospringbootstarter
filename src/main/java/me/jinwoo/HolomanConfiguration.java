6da3512b52ccbef453af7ba595df974c31e6da77
jinwoo park <swat018@gmail.com>
2021-07-18 17:26:04 +0900

    스프링 부트 개념과 활용 - 자동설정만들기 2부: @ConfigurationProperties

--- src/main/java/me/jinwoo/HolomanConfiguration.java
+++ src/main/java/me/jinwoo/HolomanConfiguration.java
@@ -1,16 +1,21 @@
 package me.jinwoo;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(HolomanProperties.class)
 public class HolomanConfiguration {
 
     @Bean
-    public Holoman holoman() {
+    @ConditionalOnMissingBean
+    public Holoman holoman(HolomanProperties properties) {
         Holoman holoman = new Holoman();
-        holoman.setHowLong(5);
-        holoman.setName("jinwoo");
+        holoman.setHowLong(properties.getHowLong());
+        holoman.setName(properties.getName());
         return holoman;
     }
 }
